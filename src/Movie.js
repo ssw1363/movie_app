5fd703225c12ce91459b71b4f843c4c73ec4810c
SEO SEUNGWAN <ssw1363@naver.com>
2020-01-18 00:58:01 +0900

    third commit for react

--- src/Movie.js
+++ src/Movie.js
@@ -3,14 +3,19 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 
 
-function Moive({ year, title, summary, poster}){
+function Moive({ year, title, summary, poster, genres}){
     return (
-    <div class="movies__movie">
+    <div className="movie">
         <img src={poster} alt={title} title={title} />
-        <div class="movie__data">
-        <h3 class="movie__title">{title}</h3>
-        <h5 class="movie__year">{year}</h5>
-        <p class="movie__summery">{summary}</p>
+        <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+                <ul className="genres">
+                    {genres.map((genre, index) => (
+                    <li key={index} className="movie__genre">{genre}</li>
+                    ))}
+                </ul>
+        <p className="movie__summery">{summary.slice(0,180)}...</p>
         </div>
     </div>
     );
@@ -21,7 +26,8 @@ Moive.propTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export default Moive;
