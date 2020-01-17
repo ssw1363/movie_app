302da374e270369c2be9f8385563ddb3187598d1
SEO SEUNGWAN <ssw1363@naver.com>
2020-01-18 00:55:04 +0900

    Updates

---
+++ precache-manifest.8df207f05c9bcd6c9ad559f7baec4b70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "792355d972787946f947afd5dd852dd7",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "10ee4440cf98876accb7",
+    "url": "/movie_app/static/css/main.33f8564a.chunk.css"
+  },
+  {
+    "revision": "f61bdc7f7bc08ddc47c9",
+    "url": "/movie_app/static/js/2.6efedadd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app/static/js/2.6efedadd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10ee4440cf98876accb7",
+    "url": "/movie_app/static/js/main.7c9cb467.chunk.js"
+  },
+  {
+    "revision": "ca4b155c3570fabe1dd4",
+    "url": "/movie_app/static/js/runtime-main.98d66c69.js"
+  }
+]);
